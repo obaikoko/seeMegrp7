6bb532841b3891fa64f7a0e6d628d1c6aa3680a8
Niz46 <paujohn66@gmail.com>
2024-05-20 12:17:41 +0100

    favour-commit

--- pages/addfriends.jsx
+++ pages/addfriends.jsx
@@ -1,30 +1,45 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import Particle from '../components/design';
-import styles from '../styles/addfriends.module.css';
-import { useGetUsersQuery } from '@/src/features/auth/userApiSlice';
-import Loader from '@/components/Loader';
-// import AddFriendBtn from '@/components/AddFriendBtn';
-import { useAddFriendMutation } from '@/src/features/auth/friendsApiSlice';
-import { toast } from 'react-toastify';
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Particle from "../components/design";
+import styles from "../styles/addfriends.module.css";
+import { useGetUsersQuery } from "@/src/features/auth/userApiSlice";
+import Loader from "@/components/Loader";
+import { useAddFriendMutation } from "@/src/features/auth/friendsApiSlice";
+import { toast } from "react-toastify";
 
 function AddFriends() {
   const router = useRouter();
-
+  const [requestedUsers, setRequestedUsers] = useState([]);
+  const [allRequestsSent, setAllRequestsSent] = useState(false);
   const [addFriend, { isLoading: loadingAddFriend, error: addFriendError }] =
     useAddFriendMutation();
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
 
+  useEffect(() => {
+    if (users && requestedUsers.length === users.length) {
+      setAllRequestsSent(true);
+    }
+  }, [requestedUsers, users]);
+
   const handleSkipButton = () => {
-    router.push('/call');
+    router.push("/call");
   };
+
   const handleRequestBtn = async (id) => {
     try {
-      const res = await addFriend({ recipient: id }).unwrap();
-      toast.success('Request sent successfully');
+      await addFriend({ recipient: id }).unwrap();
+      setRequestedUsers([...requestedUsers, id]);
+      toast.success("Request sent successfully");
     } catch (err) {
       toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  const handleContinue = (e) => {
+    if (!allRequestsSent) {
+      e.preventDefault();
+    } else {
+      handleSkipButton();
     }
   };
 
@@ -32,10 +47,10 @@ function AddFriends() {
     <>
       <Particle />
       <div className={styles.containerAddfriend}>
-        <div className={styles['parent-div2']}>
+        <div className={styles["parent-div2"]}>
           <h1>Add Friends</h1>
         </div>
-        <div className={styles['parent-div']}>
+        <div className={styles["parent-div"]}>
           <div className={styles.cally}>
             <h3 className={styles.header}>Suggested</h3>
             {isLoading && <Loader />}
@@ -45,14 +60,22 @@ function AddFriends() {
                 users.map((user) => (
                   <div key={user._id}>
                     <img
-                      src={user.image?.url || user.iamge}
-                      alt=''
-                      width={50}
-                      height={50}
+                      src={user.image?.url || user.image}
+                      alt="Avatar"
                     />
-                    {user.username}{' '}
-                    <button onClick={() => handleRequestBtn(user._id)}>
-                      ADD
+                    {user.username}{" "}
+                    <button
+                      onClick={() => handleRequestBtn(user._id)}
+                      disabled={requestedUsers.includes(user._id)}
+                      style={
+                        requestedUsers.includes(user._id)
+                          ? { backgroundColor: "rgba(8, 72, 125, 0.5)", color: "white" }
+                          : {}
+                      }
+                    >
+                      {requestedUsers.includes(user._id)
+                        ? "Request Sent"
+                        : "ADD"}
                     </button>
                   </div>
                 ))}
@@ -63,12 +86,19 @@ function AddFriends() {
             <div className={styles.con1}>
               <h3>Add by Username or ID</h3>
               <label>
-                <input type='number' placeholder='' required />
+                <input type="number" placeholder="" required />
               </label>
+              <button onClick={handleContinue}>Add</button>
             </div>
             <div className={styles.con2}>
-              <img src='https://res.cloudinary.com/duz7maquu/image/upload/v1716041164/SeeMe/Layer_2_rzzmxu.svg' />
-              <a href='#'>Continue</a>
+              <img src="https://res.cloudinary.com/duz7maquu/image/upload/v1716041164/SeeMe/Layer_2_rzzmxu.svg" />
+              <a
+                href="#"
+                className={allRequestsSent ? styles.active : styles.inactive}
+                onClick={handleContinue}
+              >
+                Continue
+              </a>
               <button onClick={handleSkipButton}>Skip</button>
             </div>
           </div>
