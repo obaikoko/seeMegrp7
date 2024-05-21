f3c03e79e7b23f20cc094e19b41621e67c10bbc7
obaikoko <jesseobinna7@gmail.com>
2024-05-21 12:26:08 +0100

    updated

--- pages/updatefriends.js
+++ pages/updatefriends.js
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import Particle from "../components/design";
-import styles from "../styles/addfriends.module.css";
-import { useGetUsersQuery } from "@/src/features/auth/userApiSlice";
-import Loader from "@/components/Loader";
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import Particle from '../components/design';
+import styles from '../styles/addfriends.module.css';
+import { useGetUsersQuery } from '@/src/features/auth/userApiSlice';
+import Loader from '@/components/Loader';
 import {
   useGetFriendQuery,
   useUpdateFriendsRequestMutation,
-} from "@/src/features/auth/friendsApiSlice";
-import { toast } from "react-toastify";
-
-
+} from '@/src/features/auth/friendsApiSlice';
+import { toast } from 'react-toastify';
 
 function UpdateFriends() {
   const router = useRouter();
@@ -30,7 +28,7 @@ function UpdateFriends() {
     try {
       const res = await updateFriendsRequest({
         sender: id,
-        status: "accepted",
+        status: 'accepted',
       }).unwrap();
       refetch();
       toast.success(res);
@@ -42,7 +40,7 @@ function UpdateFriends() {
     try {
       const res = await updateFriendsRequest({
         sender: id,
-        status: "rejected",
+        status: 'rejected',
       }).unwrap();
       refetch();
       toast.success(res);
@@ -55,10 +53,10 @@ function UpdateFriends() {
     <>
       <Particle />
       <div className={styles.containerAddfriend}>
-        <div className={styles["parent-div2"]}>
+        <div className={styles['parent-div2']}>
           <h1>Friend Request</h1>
         </div>
-        <div className={styles["parent-div"]}>
+        <div className={styles['parent-div']}>
           <div className={styles.cally}>
             <h3 className={styles.header}>Pending Request</h3>
             {isLoading && <Loader />}
@@ -67,15 +65,14 @@ function UpdateFriends() {
               {usersFriends &&
                 usersFriends.map((user) => (
                   <div key={user._id}>
-                    {/* <img
-                      src={user.image?.url || user.iamge}
+                    <img
+                      className={styles.userImg}
+                      src={user.sender.image?.url || user.sender.image}
                       alt=''
-                      width={50}
-                      height={50}
-                    /> */}
-                    {user.username}{" "}
+                    />
+                    {user.sender.username}{' '}
                     <button onClick={() => acceptRequestBtn(user.sender)}>
-                      {user.status === "accepted" ? "accepted" : "Accept"}
+                      {user.status === 'accepted' ? 'accepted' : 'Accept'}
                     </button>
                     <button onClick={() => rejectRequestBtn(user.sender)}>
                       Reject
@@ -83,18 +80,6 @@ function UpdateFriends() {
                   </div>
                 ))}
             </>
-          </div>
-
-          <div className={styles.josh}>
-            <div className={styles.con1}>
-              <h3>Add by Username or ID</h3>
-              <label>
-                <input type="number" placeholder="" required />
-              </label>
-            </div>
-            <div className={styles.con2}>
-              <img src="https://res.cloudinary.com/duz7maquu/image/upload/v1716041164/SeeMe/Layer_2_rzzmxu.svg" />
-            </div>
           </div>
         </div>
       </div>
