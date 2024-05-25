0f9ae87caaa4054ad67a746b7ac47fe17648c0dc
Niz46 <paujohn66@gmail.com>
2024-05-25 08:45:16 +0100

    favour-commit

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
+import Auth from './auth';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
-      <h1>SeeMe</h1>
+      <Auth />
     </>
   );
 }
